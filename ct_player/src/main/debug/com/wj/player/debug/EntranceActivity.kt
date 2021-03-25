5c168ef6c57dd0b0e9c17cf9f08455ab3652b0cc
linux_link <cilingciling2014@outlook.com>
2021-03-26 01:13:37 +0800

    MagneticDog: add video ui

--- ct_player/src/main/debug/com/wj/player/debug/EntranceActivity.kt
+++ ct_player/src/main/debug/com/wj/player/debug/EntranceActivity.kt
@@ -7,6 +7,6 @@ import com.wj.player.R
 class EntranceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_entrance)
+        setContentView(R.layout.layout_video_landscape)
     }
 }
